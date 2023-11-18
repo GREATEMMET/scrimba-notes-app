91c86d0d314c2d811d7258596c0cd3fdbb9d201a
Greatemmet <greatemmet@gmail.com>
2023-11-18 21:14:06 +0100

    reset vite and created Editor and Sidebar components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import Editor from './components/Editor'
+
+function App() {
+  const app = ReactMde
+
+
+
+  return(
+    <div>
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
